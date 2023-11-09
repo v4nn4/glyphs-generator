9e4477efdca3576d6b643896b33ac66785e5cdb3
v4nn4 <romflorentz@gmail.com>
2023-11-09 23:46:36 +0100

    Unsorted rasterize

--- glyphs_generator/glyph.py
+++ glyphs_generator/glyph.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from functools import reduce
 from typing import Dict, List, Tuple, Callable
 
 import matplotlib.pyplot as plt
@@ -61,13 +62,13 @@ class Glyph:
     def __eq__(self, other: "Glyph") -> bool:
         return np.abs(self.rasterize(5) - other.rasterize(5)).sum() == 0
 
-    def rasterize(self, dimension: int) -> np.ndarray:
-        array = np.zeros(shape=(dimension, dimension))
+    def rasterize(self, glyph_size: int) -> np.ndarray:
+        array = np.zeros(shape=(glyph_size, glyph_size))
 
         def world_to_view(coordinate: Coordinate) -> Tuple[int, int]:
             return int(
-                np.round(coordinate.x * (dimension - 1) / 2 + (dimension - 1) / 2)
-            ), int(np.round(coordinate.y * (dimension - 1) / 2 + (dimension - 1) / 2))
+                np.round(coordinate.x * (glyph_size - 1) / 2 + (glyph_size - 1) / 2)
+            ), int(np.round(coordinate.y * (glyph_size - 1) / 2 + (glyph_size - 1) / 2))
 
         for stroke in self.strokes:
             x0, y0 = world_to_view(stroke.start)
@@ -75,13 +76,16 @@ class Glyph:
             array[x0, y0] = 1
             array[x1, y1] = 1
             if x0 == x1:
-                for y in range(min(y0, y1), min(max(y0, y1) + 1, dimension)):
+                for y in range(min(y0, y1), min(max(y0, y1) + 1, glyph_size)):
                     array[x0, y] = 1
             elif y0 == y1:
-                for x in range(min(x0, x1), min(max(x0, x1) + 1, dimension)):
+                for x in range(min(x0, x1), min(max(x0, x1) + 1, glyph_size)):
                     array[x, y0] = 1
             else:
-                raise Exception("Slope is not implemented")
+                slope = (y1 - y0) / (x1 - x0)
+                for x in range(min(x0, x1), min(max(x0, x1) + 1, glyph_size)):
+                    y = int(np.floor(slope * (x - x0) + y0))
+                    array[x, y] = 1
         return array
 
     def intersect(self, stroke: Stroke) -> bool:
@@ -111,22 +115,48 @@ class Glyph:
 class Alphabet:
     glyphs: Dict[int, List[Glyph]]
 
-    def rasterize(self, dimension: int = 5, margin: int = 1) -> np.ndarray:
-        largest_number_of_glyphs_per_order = np.max(
-            np.array([len(glyphs) for glyphs in self.glyphs.values()])
-        )
-        strokes = self.glyphs[len(self.glyphs) - 1][0].strokes
-        tensor = np.zeros(
-            shape=(
-                (dimension + margin) * len(strokes),
-                (dimension + margin) * largest_number_of_glyphs_per_order,
+    def rasterize(
+        self,
+        glyph_size: int = 5,
+        margin: int = 1,
+        sorted: bool = True,
+        nb_glyphs_per_row: int = 5,
+    ) -> np.ndarray:
+        if sorted:
+            largest_number_of_glyphs_per_order = np.max(
+                np.array([len(glyphs) for glyphs in self.glyphs.values()])
             )
-        )
-        for order, glyphs in self.glyphs.items():
-            for i, glyph in enumerate(glyphs):
+            strokes = self.glyphs[len(self.glyphs) - 1][0].strokes
+            tensor = np.zeros(
+                shape=(
+                    (glyph_size + margin) * len(strokes),
+                    (glyph_size + margin) * largest_number_of_glyphs_per_order,
+                )
+            )
+            for order, glyphs in self.glyphs.items():
+                for i, glyph in enumerate(glyphs):
+                    tensor[
+                        (glyph_size + margin) * order : (glyph_size + margin) * order
+                        + glyph_size,
+                        (glyph_size + margin) * i : (glyph_size + margin) * i
+                        + glyph_size,
+                    ] = glyph.rasterize(glyph_size)
+        else:
+            nb_glyphs = np.array([len(glyphs) for glyphs in self.glyphs.values()]).sum()
+            nb_glyphs_per_columns = int(np.ceil(nb_glyphs / nb_glyphs_per_row))
+            strokes = self.glyphs[len(self.glyphs) - 1][0].strokes
+            tensor = np.zeros(
+                shape=(
+                    (glyph_size + margin) * nb_glyphs_per_columns,
+                    (glyph_size + margin) * nb_glyphs_per_row,
+                )
+            )
+            all_glyphs = reduce(list.__add__, self.glyphs.values())
+            for k, glyph in enumerate(all_glyphs):
+                j = k % nb_glyphs_per_row
+                i = k // nb_glyphs_per_row
                 tensor[
-                    (dimension + margin) * order : (dimension + margin) * order
-                    + dimension,
-                    (dimension + margin) * i : (dimension + margin) * i + dimension,
-                ] = glyph.rasterize(dimension)
+                    (glyph_size + margin) * i : (glyph_size + margin) * i + glyph_size,
+                    (glyph_size + margin) * j : (glyph_size + margin) * j + glyph_size,
+                ] = glyph.rasterize(glyph_size)
         return tensor
