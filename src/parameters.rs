2ac9cb1615bd3e1f072a55cdf65543be7b24822c
v4nn4 <romflorentz@gmail.com>
2024-04-09 08:42:10 +0200

    Move Rust code to glyphs-generator-rs

--- src/parameters.rs
+++
@@ -1,9 +0,0 @@
-use crate::stroke::Stroke;
-use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub struct Parameters {
-    pub parent_strokes: Vec<Stroke>,
-    pub intersection_matrix: Vec<Vec<u8>>,
-    pub transformation_matrix: Vec<Vec<usize>>,
-}
